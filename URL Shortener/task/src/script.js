241a7db3ef13e8d2cae5f3fdbc0b19fa51e803bc
Tomáš Kaňuch <tomas.kanuch@gmail.com>
2025-04-11 19:52:33 +0200

    Stage 2 - Count the clicks

--- URL Shortener/task/src/script.js
+++ URL Shortener/task/src/script.js
@@ -1,37 +1,30 @@
-// Get input element
-const input = document.getElementById("input-url");
-// Get list element
-const listURL = document.getElementById("list-url");
-// Get button element
-let createBtn = document.getElementById("button-create");
-//Get error message <p> element
-let errorMessage = document.getElementById("error-message");
-// Check validity of input URL link
-function checkURL() {
-    let userInput = input.value;
-    // check input value with RegExp value vor validation
-    if (/^(http(s)?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-.\/?%:#&=]*)?$/.test(userInput) === true) {
+let input = document.getElementById('input-url');
+let urlList = document.getElementById('list-url');
+let errorMessage = document.getElementById('error-message');
+
+function inputAction() {
+    let url = input.value;
+    if (/^(http(s)?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-.\/?%:#&=]*)?$/.test(url) === true) {
         const createList = document.createElement('li');
-        createShortURL(createList);
-        listURL.appendChild(createList);
+        createList.innerHTML = getRandomShorturl();
+        urlList.appendChild(createList);
     } else {
         errorMessage.textContent = "Please enter a valid url";
     }
 }
-// Create short-link
-function createShortURL(parent) {
-    let shortUrl = '';
+
+function getRandomShorturl() {
+    let text = '';
     let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     for (let i = 0; i < 5; i++)
-        shortUrl += possible.charAt(Math.floor(Math.random() * possible.length));
-    // create link (<a>) element
-    const anchor = document.createElement('a');
-    // set attributes for new link
-    anchor.href = input.value;
-    anchor.target = '_blank';
-    anchor.innerText = `localhost/${shortUrl}`;
-    // create <span> element with actual link, which will be appended to shortURL in list item
-    const textNode = document.createTextNode(` - ${input.value}`);
-    parent.appendChild(anchor);
-    parent.appendChild(textNode);
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return `<a target='_blank' href='${input.value}' >localhost/${text}</a> - ${input.value}<span> - Clicks: 0</span>`;
 }
+
+document.getElementById("list-url").addEventListener("click", function(e) {
+    let counterElement = e.target.nextElementSibling;
+    let countNumber = counterElement.innerText.at(11);
+    let counter = parseInt(countNumber);
+    counter++;
+    counterElement.innerText = ' - Clicks: ' + counter;
+})
